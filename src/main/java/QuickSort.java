e0128a2b2ed36bd0b36c1a9e534bc621e327b50b
perekhod <perekhod@mail.ru>
2017-02-07 15:58:30 +0300

    комментарии

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -3,16 +3,20 @@
  */
 class QuickSort {
 
+
     static void sort(Comparable[] ints) {
         sort(ints, 0, ints.length - 1);
     }
 
     private static void sort(Comparable[] array, int begin, int end) {
+        //1. база рекурсии - 0 или 1
         int len = (end - begin) + 1;
         if (len == 0 || len == 1) {
             return;
         }
 
+
+        //2. база рекурсии - 2
         if (len == 2) {
             if (array[begin].compareTo(array[end]) == 1) {
                 Comparable tmp = array[begin];
@@ -23,11 +27,13 @@ class QuickSort {
         }
 
 
+        //3. первоначальный индексы и база
         int base = begin + (len / 2);
         int i1 = begin;
         int i2 = end;
 
 
+        //4. цикл
         while (true) {
 
             boolean leftIsBase = i1 == base;
@@ -35,10 +41,12 @@ class QuickSort {
             boolean leftIsReady = array[i1].compareTo(array[base]) == 1;
             boolean rightIsReady = array[i2].compareTo(array[base]) == -1;
 
+            //оба индексы долшли до базы
             if (leftIsBase && rightIsBase) {
                 break;
             }
 
+            //обмен
             if ((leftIsReady && rightIsReady) || (leftIsReady && rightIsBase) || (rightIsReady && leftIsBase)) {
                 Comparable tmp = array[i1];
                 array[i1] = array[i2];
@@ -52,6 +60,7 @@ class QuickSort {
                 continue;
             }
 
+            //смещение индексов
             if (!leftIsReady && !leftIsBase) {
                 i1++;
             }
@@ -62,6 +71,7 @@ class QuickSort {
 
         }
 
+        //5. рекурсия
         sort(array, begin, base);
         sort(array, base + 1, end);
 
