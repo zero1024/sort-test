fe59b68af41b1084ce15a40b431777a30056529b
perekhod <perekhod@mail.ru>
2017-02-16 12:04:08 +0300

    поправил тесты

--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -3,11 +3,14 @@ package sort;
 import org.junit.Test;
 import tree.BiTree;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
+import static java.util.Arrays.copyOf;
 
 /**
  * @author Perekhod Oleg
@@ -16,16 +19,30 @@ public class SortTest {
 
     @Test
     public void sort() throws Exception {
-        List<Integer> intList = asList(111, 0, 1, 2, 3, 4, 5, 6, 7, 10, 111, 101, 1, 2, 5, 111, 101, 102);
+        Random random = new Random();
+        List<Integer[]> arrays = Stream
+                .generate(() -> Stream.generate(random::nextInt).limit(100 + random.nextInt(500)).toArray(Integer[]::new))
+                .limit(20)
+                .collect(Collectors.toList());
+
         for (Sort<Integer> sort : sorts()) {
-            for (int i = 0; i < 100; i++) {
-                Collections.shuffle(intList);
-                Integer[] array = new Integer[intList.size()];
-                sort.sort(intList.toArray(array));
-                assert Arrays.equals(array, new Integer[]{0, 1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 10, 101, 101, 102, 111, 111, 111});
+            for (Integer[] array : arrays) {
+                assert asList(sort(sort, array)).equals(expectedList(array));
             }
         }
 
+    }
+
+    private static List<Integer> expectedList(Integer[] array) {
+        List<Integer> expectedList = asList(array);
+        Collections.sort(expectedList);
+        return expectedList;
+    }
+
+    private static Integer[] sort(Sort<Integer> sort, Integer[] array) {
+        Integer[] copyArray = copyOf(array, array.length);
+        sort.sort(copyArray);
+        return copyArray;
     }
 
 
